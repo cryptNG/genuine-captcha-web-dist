2afd8ee24c2e0f01267bdbb3d1ee044f082b18db
Ramazan Yavuz <info@yavuz.support>
2025-03-21 15:17:43 +0100

    update genuine captcha

--- hl/README.md
+++
@@ -1,3 +0,0 @@
-This is a directory for hardlink files
-
- ln ~/ai-term-vanillajs/ai-term.js ~/genuine-captcha/sources/genuine-captcha-web/public/hl/ai-term.js
--- hl/ai-term-setup-53f128c57902343214118563f10db550.js
+++
@@ -1,3 +0,0 @@
-let _aiTermSetup=function(){}
-"undefined"!=typeof document&&(_aiTermSetup=function(){"undefined"!=typeof document&&(null!==document.querySelector("template#ai-term")&&console.log("Found <template>: ai-term"),null!==document.querySelector("template#ai-term-chat")&&console.log("Found <template>: ai-term-chat"))})
-export default _aiTermSetup
